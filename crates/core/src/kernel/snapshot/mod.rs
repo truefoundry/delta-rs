cc884944daaa8a02430ff6f23239329ab64b233f
Chirag Jain <jain.chirag925@gmail.com>
2025-10-28 12:53:11 +0530

    Make commit info public

--- crates/core/src/kernel/snapshot/mod.rs
+++ crates/core/src/kernel/snapshot/mod.rs
@@ -296,7 +296,7 @@ impl Snapshot {
     ///
     /// A stream of commit infos.
     // TODO: move outer error into stream.
-    pub(crate) async fn commit_infos(
+    pub async fn commit_infos(
         &self,
         log_store: &dyn LogStore,
         limit: Option<usize>,
@@ -554,7 +554,7 @@ impl EagerSnapshot {
     }
 
     /// Get the underlying snapshot
-    pub(crate) fn snapshot(&self) -> &Snapshot {
+    pub fn snapshot(&self) -> &Snapshot {
         &self.snapshot
     }
 
